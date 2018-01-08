40937461a5175f0446465078a11dd7b09d9f50e6
NeoPlace team <rorol@gmail.com>
2018-01-08 11:11:47 +0100

    NPT-102 Init project

---
+++ src/test/java/com/neoplace/app/NeoPlaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neoplace.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeoPlaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

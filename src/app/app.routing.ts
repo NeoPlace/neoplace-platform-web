2fa7dda26d037c24d57b3d4d5d843321e52f6d60
Roland <roland@neoplace.io>
2018-06-30 15:47:24 +0200

    Update route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,31 @@
 import {Routes} from '@angular/router';
+import {AdminLayoutComponent} from './shared/components/layouts/admin-layout/admin-layout.component';
 
 export const rootRouterConfig: Routes = [
-
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: './views/home/home.module#HomeModule',
+        data: { title: 'Home', breadcrumb: 'HOME' }
+      },
+      {
+        path: 'dashboard',
+        loadChildren: './views/dashboard/dashboard.module#DashboardModule',
+        data: { title: 'Dashboard', breadcrumb: 'DASHBOARD'}
+      },
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'sessions/404'
+  }
 ];
 
